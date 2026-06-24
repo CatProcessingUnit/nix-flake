2e2398bbb13fed9c9bfc974ed54afa555ccc4a2d
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-06-24 16:02:02 +0200

    replaced zram size with zram memoryPercent, added zram to laptop host

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -6,4 +6,9 @@
    boot.kernelPackages = pkgs.linuxPackages_zen;
    features.samba.enable = true;
    system.stateVersion = "26.05";
+
+   features.zram = {
+	enable = true;
+	memoryPercent = 100;
+   };
 }
--- modules/features/zram.nix
+++ modules/features/zram.nix
@@ -3,17 +3,17 @@
 {
    options = {
 	features.zram.enable = lib.mkEnableOption "Enable zram";
-	features.zram.size = lib.mkOption {
+	features.zram.memoryPercent = lib.mkOption {
 		type = lib.types.int;
-		description = "zram size (in GB)";
-		default = 8;
+		description = "zram size (%)";
+		default = 50;
 	};
    };
    config = lib.mkIf config.features.zram.enable {
 	zramSwap = {
 		enable = true;
 		algorithm = "zstd";
-		memoryMax = config.features.zram.size * 1024 * 1024 * 1024;
+		memoryPercent = config.features.zram.memoryPercent;
 		priority = 10;
 	};
    };
