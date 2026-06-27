4ff62333e5c77843c1af8f607a668d532a6b3159
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-06-27 22:14:04 +0200

    remove requiredFileCheck function
    mkHost would already throw an error when one of the files was missing

--- myLib/getAllHosts.nix
+++ myLib/getAllHosts.nix
@@ -3,28 +3,15 @@
 {self, lib, flakePaths, ...}:
 
 let
-   requiredFiles = [ "configuration.nix" "hardware-configuration.nix" ];
-
-   requiredFileCheck = hostPath: (
-   	builtins.all 
-		(fileName: 
-			let
-				filePath = (hostPath + "/${fileName}");
-			in 
-				if builtins.pathExists (hostPath + "/${fileName}") then true
-				else throw "${filePath} is missing!"
-		) 
-		requiredFiles
-	);
-
-   isValidHost = name: type:
+   isDirectory = name: type:
    	if type == "directory" then
-		requiredFileCheck (flakePaths.hosts + "/${name}")
+		true
 	else builtins.trace "${name} is not an directory, skipping" false;
 
    hostDirectoryContents = builtins.readDir flakePaths.hosts;
-   validHosts = lib.filterAttrs isValidHost hostDirectoryContents;
+   validHosts = lib.filterAttrs isDirectory hostDirectoryContents;
 
+   # get names from validHosts attrSet
    getAllHosts = builtins.attrNames validHosts;
 in
    getAllHosts
