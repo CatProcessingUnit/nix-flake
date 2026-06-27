a7be1b03c968ae37f427bf4c5c72af724e4c8185
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-06-28 01:15:02 +0200

    automatically load hm modules
    so I wont need to add the entries manually when adding new users

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,9 +1,39 @@
-{config, lib, pkgs, ...}:
+{config, lib, pkgs, flakePaths, ...}:
 
 {
   imports = [
-    ./test.nix
+    ./users/test.nix
   ];
 
   users.test.enable = lib.mkDefault true;
+
+  # create home-manager module entries for existing users
+  # by reading the contents of users directory
+  # ignore if user doesn't have a home-manager module
+  
+  # it removes the need for adding the entries manually
+  home-manager.users = let
+  	removeFileExtension = name: lib.strings.removeSuffix name ".nix";
+	getHomeFolderPath = username: (flakePaths.home + "/${username}");
+	hasHomeFolder = username: builtins.pathExists (getHomeFolderPath username);
+	isModule = name: type: 
+		let
+			nameLength = builtins.stringLength name;
+		in
+			if (type == "regular") && ((builtins.substring (nameLength - 4) 4 name) == ".nix") then true
+			else builtins.trace "${name} is not a module" false;
+
+
+	userDirectoryContent = builtins.readDir ./users;
+	userModules = builtins.attrNames (lib.filterAttrs isModule userDirectoryContent);
+	userNames = map removeFileExtension userModules;
+	usersWithHomeModules = lib.foldr 
+		# if user has home folder then
+		# return acc with it's added username
+		# else just return acc
+		(userName: acc: if (hasHomeFolder userName) then acc ++ [userName] else acc)
+		[] userNames;
+	attrList = map (username: {name = username; value = import ((getHomeFolderPath username) + "/home.nix");}) usersWithHomeModules;
+	attrSet = builtins.listToAttrs attrList;
+  in attrSet;
 }
