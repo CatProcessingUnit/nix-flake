d64fe61c22f35c1ef4b71045c5c3a043802407dd
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-07-09 17:26:46 +0200

    add proton-ge

--- modules/features/gaming.nix
+++ modules/features/gaming.nix
@@ -10,6 +10,9 @@
 			enable = true;
 			remotePlay.openFirewall = true;
 			dedicatedServer.openFirewall = true;
+			extraCompatPackages = with pkgs; [
+				proton-ge-bin
+			];
 		};
 		gamemode = {
 			enable = true;
