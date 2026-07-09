1237e4de30f5a0ea719f14fe5616d93edd5310f8
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-07-09 15:42:11 +0200

    enable user

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -6,6 +6,9 @@
 	displayProtocol = "x11";
 	displayManager = "lightDM";
    };
+   users = {
+	jac.enable = true;
+   };
    boot.kernelPackages = pkgs.linuxPackages_zen;
    features.samba.enable = true;
    system.stateVersion = "26.05";
