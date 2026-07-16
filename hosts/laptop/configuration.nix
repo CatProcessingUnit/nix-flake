5c141b8131682b35c731196428b21499f15ab628
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-07-16 21:41:56 +0200

    fix laptop host for new module layout

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,20 +1,21 @@
 {config, pkgs, ...}:
 
 {
-   desktop = {
-	env = "i3";
-	displayProtocol = "x11";
-	displayManager = "ly";
-   };
-   users = {
-	jac.enable = true;
+   myFlake = {
+	   desktop = {
+		env = "i3";
+		displayProtocol = "x11";
+		displayManager = "ly";
+	   };
+	   users = {
+		jac.enable = true;
+	   };
+	   features.zram = {
+		enable = true;
+		memoryPercent = 100;
+	   };
    };
    boot.kernelPackages = pkgs.linuxPackages_zen;
    features.samba.enable = true;
    system.stateVersion = "26.05";
-
-   features.zram = {
-	enable = true;
-	memoryPercent = 100;
-   };
 }
