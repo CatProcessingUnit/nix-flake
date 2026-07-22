5abc15814d223c5a84a6c86d9f5883d6fcab38e4
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-07-23 00:22:05 +0200

    firejail local profiles

--- modules/features/firejail/default.nix
+++ modules/features/firejail/default.nix
@@ -10,5 +10,24 @@ in {
 	programs.firejail = {
 		enable = true;
 	};
+
+	# local profiles
+	environment.etc = let
+		profiles = (builtins.filter
+			(e: lib.strings.hasSuffix ".local" e)
+			(map
+				(e: e.name)
+				(lib.attrsToList (builtins.readDir ./localProfiles)))
+		);
+	in
+	builtins.listToAttrs
+		(map
+			(e: {
+				name = "firejail/${e}"; 
+				value = {
+					text = builtins.readFile (./localProfiles + "/${e}");
+				};
+			})
+			profiles);
    };
 }
