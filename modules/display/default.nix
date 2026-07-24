bc2d9efa0d7c1adce3acc823f507f01e54c9f6ab
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-07-24 21:13:42 +0200

    mkModuleSelect function

--- modules/display/default.nix
+++ modules/display/default.nix
@@ -1,19 +1,25 @@
 {config, lib, pkgs, myLib, ...}:
 
-{
+let
+	mkModuleSelect = path: lib.types.enum
+		(map 
+			(e: lib.strings.removeSuffix ".nix" e.name) 
+			(lib.attrsToList (builtins.readDir path))
+		);
+in {
 	options.myFlake.desktop = {
 		env = lib.mkOption {
-			type = lib.types.nullOr lib.types.str;
+			type = lib.types.nullOr (mkModuleSelect ./gui);
 			description = "select DE";
 			default = builtins.null;
 		};
 		displayProtocol = lib.mkOption {
-			type  = lib.types.nullOr (lib.types.enum ["x11" "wayland"]);
+			type  = lib.types.nullOr (mkModuleSelect ./displayProtocol);
 			description = "select display protocol";
 			default = builtins.null;
 		};
 		displayManager = lib.mkOption {
-			type = lib.types.nullOr lib.types.str;
+			type = lib.types.nullOr (mkModuleSelect ./displayManagers);
 			description = "select display manager";
 			default = builtins.null;
 		};
