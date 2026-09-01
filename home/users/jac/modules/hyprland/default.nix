6002fef1cb13c09019d58bc989127fc99779a748
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-09-01 14:40:45 +0200

    remove unused option

--- home/users/jac/modules/hyprland/default.nix
+++ home/users/jac/modules/hyprland/default.nix
@@ -16,40 +16,6 @@
 				};
 			};
 		};
-		wayland.windowManager.hyprland = {
-			#enable = true;
-			#package = null;
-			extraConfig = ''
-				--local env = {
-				--	{"LIBVA_DRIVER_NAME", "nvidia"},
-				--	{"__GLX_VENDOR_LIBRARY_NAME", "nvidia"}
-				--}
-				hl.config({
-					general = {
-						layout = "scroll"
-					}
-				})
-
-				local mainMod = "SUPER"
-				local binds = {
-					{mainMod.." + R", hl.dsp.exec_cmd("rofi -show drun")},
-					{mainMod.." + Q", hl.dsp.window.close()},
-					{mainMod.." + T", hl.dsp.exec_cmd("kitty")}
-				}
-
-				for k,v in pairs(binds) do
-					hl.bind(v[1],v[2])		
-				end
-				--hl.bind(
-				--	"SUPER + F",
-				--	hl.dsp.exec_cmd("rofi -show drun")
-				--)
-				--hl.on("hyprland.start", function ()
-				--	hl.exec_cmd("hyprpaper")
-				--end)
-			'';
-
-		};
 		services = {
 			swaync = {
 				enable = true;
