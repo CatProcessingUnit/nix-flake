0f83387ce2e0221e470e425e705e3772dfb24459
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-09-13 00:55:32 +0200

    remove options
    now defined in default.nix

--- modules/features/swap/zram.nix
+++ modules/features/swap/zram.nix
@@ -4,19 +4,12 @@
 let
 	cfg = config.myFlake.features.swap.${moduleInfo.name};
 in {
-   options.myFlake.features.swap.${moduleInfo.name} = {
-	memoryPercent = lib.mkOption {
-		type = lib.types.int;
-		description = "zram size (%)";
-		default = 50;
-	};
-   };
-   config = lib.mkIf cfg.enable {
-	zramSwap = {
-		enable = true;
-		algorithm = "zstd";
-		memoryPercent = cfg.memoryPercent;
-		priority = 10;
-	};
-   };
+	config = lib.mkIf (cfg.enable) {
+		zramSwap = {
+			enable = true;
+			algorithm = cfg.algorithm;
+			memoryPercent = cfg.memoryPercent;
+			priority = 10;
+		};
+	   };
 }
