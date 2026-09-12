28bd26c5c0df589ed525e68e43f09d1ab90007f6
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-09-12 23:19:15 +0200

    add file swap

--- modules/features/swap/fileSwap.nix
+++ modules/features/swap/fileSwap.nix
@@ -1,9 +1,22 @@
 {moduleInfo, ...}:
-{config, ...}:
+{config, lib, ...}:
 
 let
 	cfg = config.myFlake.features.swap.${moduleInfo.name};
 in {
-	options.myFlake.features.swap.${moduleInfo.name} = {};
-	config = builtins.trace "${moduleInfo.name} not implemented" {};
+	options.myFlake.features.swap.${moduleInfo.name} = {
+		size = lib.mkOption {
+			type = lib.types.int;
+			default = 8;
+			description = "file swap size (in GB)";
+		};
+	};
+	config = lib.mkIf (cfg.enable) {		
+		swapDevices = builtins.trace "file swap enabled" [
+			{
+				device = "/.swapfile";
+				size = cfg.size*1024;
+			}
+		  ];
+	};
 }
