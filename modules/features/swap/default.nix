7ad363c494603c80b978f36fc3a17468fd1b482c
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-09-13 00:51:10 +0200

    rewrite module

--- modules/features/swap/default.nix
+++ modules/features/swap/default.nix
@@ -1,34 +1,33 @@
 {moduleInfo, ...}:
 {lib, myLib, config, ...}:
 let
-	moduleList = lib.mapAttrsToList (k: v: (lib.removeSuffix ".nix" k)) (lib.filterAttrs (k: v: k != "default.nix" && v == "regular") (builtins.readDir ./.));
+	cfg = config.myFlake.features.${moduleInfo.name};
 in {
-	options.myFlake.features.${moduleInfo.name} = (builtins.listToAttrs (
-			lib.map (
-				e: {
-					name = e;
-					value = {
-						enable = lib.mkEnableOption "enable ${e}";
-					};
-				}
-			) moduleList
-		));
-
-	config.assertions = [
-		(let
-			enabledSwapOptions = lib.lists.foldr (
-				e: count:
-					if config.myFlake.features.${moduleInfo.name}.${e}.enable then
-						count + 1
-					else
-						count
-			) 0 moduleList;
-		in {
-
-			assertion = enabledSwapOptions <= 1;
-			message = "only 1 swap option can be active at a given time";
-		})
-	];
-
+	options.myFlake.features.${moduleInfo.name} = let
+		# options shared between zram and zswap
+		sharedOptions = name: {
+			enable = lib.mkEnableOption "enable ${name}";
+			memoryPercent = lib.mkOption {
+				type = lib.types.int;
+				default = 20;
+				description = "Maximum total amount of memory that can be stored in ${name}";
+			};
+			algorithm = lib.mkOption {
+				default = "zstd";
+				description = "${name} algorithm";
+			};
+		};	
+	in {
+		zram = sharedOptions "zram";	
+		fileSwap = {
+			enable = lib.mkEnableOption "enable file swap";
+			size = lib.mkOption {
+				type = lib.types.int;
+				default = 8;
+				description = "File swap size (in GiB)";
+			};
+		};
+	};
+	
 	imports = myLib.importAllFrom ./. { inheritModuleInfo = true; };
 }
