151ecbd3ba7c85c0f8cfe051c68abba6fa6c6266
CatProcessingUnit <39676061+CatProcessingUnit@users.noreply.github.com>
2026-09-14 21:33:16 +0200

    switch devenv to stable

--- home/users/jac/home.nix
+++ home/users/jac/home.nix
@@ -1,4 +1,4 @@
-{ myLib, lib, pkgs, myFlake, ... }:
+{ myLib, lib, pkgs, pkgs-stable, myFlake, ... }:
 
 {
   imports = myLib.importAllFrom ./modules {};
@@ -30,20 +30,9 @@
      gh
      rnote
      krita
-     devenv
      libreoffice-qt
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  ] ++ [
+	pkgs-stable.devenv
   ];
   xdg.terminal-exec = {
 	enable = true;
